4b6a4f8a5fdf17570b299f31e449a6ce94d296d8
gabrielcellone <leviatan@tutanota.com>
2023-05-16 00:51:40 -0300

    Primer commit

---
+++ ensayo/acciones.js
@@ -0,0 +1,14 @@
+
+        function mostrarTexto() {
+          document.getElementById("texto_Eol").style.display = "block";
+        }
+        
+        function ocultarTexto() {
+          document.getElementById("texto_Eol").style.display = "none";
+        }
+        
+        function desplegarCancion(cancion) {
+          alert("Desplegando información de la canción: " + cancion);
+        }
+
+       
