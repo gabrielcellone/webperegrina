f46e175addbee3d9a36fd6af9c87d87ca7ec0904
gabrielcellone <leviatan@tutanota.com>
2023-05-20 12:35:19 -0300

    arreglando cambios que habáin quedado sin aceptar

--- ensayo/acciones.js
+++ ensayo/acciones.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
         function mostrarTexto() {
           document.getElementById("texto_Eol").style.display = "block";
@@ -12,19 +11,4 @@
           alert("Desplegando información de la canción: " + cancion);
         }
 
-=======
-
-        function mostrarTexto() {
-          document.getElementById("texto_Eol").style.display = "block";
-        }
-        
-        function ocultarTexto() {
-          document.getElementById("texto_Eol").style.display = "none";
-        }
-        
-        function desplegarCancion(cancion) {
-          alert("Desplegando información de la canción: " + cancion);
-        }
-
->>>>>>> 18cb08db35a301a4c47a199c53d6a10ba8760cde
        
